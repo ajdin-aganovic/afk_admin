b5c1cb1d573c66db8e5f807700de1d2fc669b4bb
Ajdin Aganović <89980063+ajdin-aganovic@users.noreply.github.com>
2023-07-11 02:46:12 +0200

    sve odrađeno

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,52 @@
-import 'package:afk_admin/providers/korisnici_provider.dart';
-import 'package:afk_admin/providers/plata_provider.dart';
-import 'package:afk_admin/providers/tip_korisnika_provider.dart';
+import 'dart:math';
+import 'dart:convert';
+import 'package:afk_admin/providers/bolest_provider.dart';
+import 'package:afk_admin/providers/clanarina_provider.dart';
+import 'package:afk_admin/providers/pozicija_provider.dart';
+import 'package:afk_admin/providers/stadion_provider.dart';
+import 'package:afk_admin/providers/statistika_provider.dart';
+import 'package:afk_admin/providers/termin_provider.dart';
+import 'package:afk_admin/providers/transakcijski_racun_provider.dart';
+import 'package:afk_admin/providers/trening_provider.dart';
+import 'package:afk_admin/screens/bolest_list_screen.dart';
+import 'package:afk_admin/screens/home_screen.dart';
+import 'package:afk_admin/screens/korisnici_list_screen.dart';
+import 'package:afk_admin/screens/reset_password_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:afk_admin/providers/korisnik_provider.dart';
+import 'package:afk_admin/providers/platum_provider.dart';
 import 'package:afk_admin/providers/uloga_provider.dart';
 import 'package:afk_admin/screens/plata_list_screen.dart';
+import 'package:afk_admin/screens/korisnik_details_screen.dart';
 import 'package:afk_admin/utils/util.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:afk_admin/widgets/makePayment.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+// import 'package:afk_admin/api/client.dart';
 
-void main() {
+import 'models/korisnik.dart';
+
+void main() async {
+  // await dotenv.load(fileName: "lib/.env");
   runApp(MultiProvider(providers:
   [
-    ChangeNotifierProvider(create: (_) => PlataProvider()),
-    ChangeNotifierProvider(create: (_) => KorisniciProvider()),
-    ChangeNotifierProvider(create: (_) => TipKorisnikaProvider()),
+    ChangeNotifierProvider(create: (_) => PlatumProvider()),
+    ChangeNotifierProvider(create: (_) => KorisnikProvider()),
     ChangeNotifierProvider(create: (_) => UlogaProvider()),
+    ChangeNotifierProvider(create: (_) => BolestProvider()),
+    ChangeNotifierProvider(create: (_) => ClanarinaProvider()),
+    ChangeNotifierProvider(create: (_) => PozicijaProvider()),
+    ChangeNotifierProvider(create: (_) => StadionProvider()),
+    ChangeNotifierProvider(create: (_) => StatistikaProvider()),
+    ChangeNotifierProvider(create: (_) => TerminProvider()),
+    ChangeNotifierProvider(create: (_) => TransakcijskiRacunProvider()),
+    ChangeNotifierProvider(create: (_) => TreningProvider()),
 
   ],
   child: const MyMaterialApp(),));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LayoutExamples(),
-    );
-  }
-}
-
-class MyHelloWorld extends StatelessWidget{
-  String title;
-  MyHelloWorld({Key?key, required this.title}):super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-
-    return Text(title);
-  }
-}
-
-class Counter extends StatefulWidget {
-  const Counter({super.key});
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _count=0;
-  void incrementCounter()
-  {
-    setState(() {
-    _count++;
-    });
-  }
-
-  void resetCounter()
-  {
-    setState(() {
-      _count=0;
-    });
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Column(
-      children: [
-        Text("You have pushed $_count times"),
-        ElevatedButton(onPressed: incrementCounter, child: const Text("Increment ++"),
-        ),
-        const Text("Press to reset the score"),
-        ElevatedButton(onPressed: resetCounter, child: const Text("Reset score"),
-        )
-      ],
-    );
-  }
-}
-
-
-class LayoutExamples extends StatelessWidget {
-  const LayoutExamples({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 150,
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text("example text"),
-        ),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text("text1"),
-            Text("text2"),
-            Text("text3"),
-          ],
-        ),
-        Container(
-          height: 150,
-          color: Colors.red,
-          alignment: Alignment.center,
-          child: const Text("example rows"),
-        ),
-      ],
-    );
-  }
-}
-
 
 class MyMaterialApp extends StatelessWidget {
   const MyMaterialApp({super.key});
@@ -150,85 +62,172 @@ class MyMaterialApp extends StatelessWidget {
   }
 }
 
-
 class LoginPage extends StatelessWidget {
  LoginPage({super.key});
 
   final TextEditingController _usernamecontroller=TextEditingController();
   final TextEditingController _passwordcontroller=TextEditingController();
 
-  late PlataProvider _plataProvider;
+  late PlatumProvider _plataProvider;
+  late KorisnikProvider _korisniciProvider;
 
   @override
   Widget build(BuildContext context) {
-    _plataProvider=context.read<PlataProvider>();
+     
+    _plataProvider=context.read<PlatumProvider>();
+    _korisniciProvider=context.read<KorisnikProvider>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),),
-        body: Center(
-          child: 
-          Container(
-            constraints: const BoxConstraints(maxHeight: 400,maxWidth: 400),
-            child: Card(
-              child: Column(children: [
-                //Image.network("https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/285px-Manchester_United_FC_crest.svg.png", height: 100,width: 100,),
-                Image.asset("assets/images/manchesterunited.png", height: 100,width: 100,),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: "Username",
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                  controller: _usernamecontroller,
-                ),
-                const SizedBox(height: 20,),
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: "Password",
-                    prefixIcon: Icon(Icons.password_outlined),
-                  ),
-                  controller: _passwordcontroller,
-                ),
-                const SizedBox(height: 20,),
-                ElevatedButton(onPressed:() async {
-                  var username=_usernamecontroller.text;
-                  var password=_passwordcontroller.text;
-                  print("login proceed u:($username) p:($password)");
-                  
-                  Authorization.username=username;
-                  Authorization.password=password;
+    return 
+        Scaffold(
+          appBar: AppBar(
+            title: const Text("Login"),),
+            body: Center(
+              child: 
+              Container(
+                constraints: const BoxConstraints(maxHeight: 400,maxWidth: 400),
+                child: Card(
+                  child: Column(children: [
+                    //Image.network("https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/285px-Manchester_United_FC_crest.svg.png", height: 100,width: 100,),
+                    // Image.asset("assets/images/manchesterunited.png", height: 100,width: 100,),
+                    const SizedBox(height: 20,),
+                    const Text('Aplikacija Fudbalskog Kluba',
+                     style: TextStyle(fontSize: 30),),
+                      const SizedBox(height: 20,),
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: "Username",
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      controller: _usernamecontroller,
+                    ),
+                    const SizedBox(height: 20,),
+                    TextField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: "Password",
+                        hintText: 'Enter your secure password',
+                        prefixIcon: Icon(Icons.password_outlined),
+                      ),
+                      controller: _passwordcontroller,
+                    ),
+                    // const SizedBox(height: 20,),
+                    // ElevatedButton(onPressed: () async{
+                      
+                    //   }),
+                    const SizedBox(height: 20,),
+                    ElevatedButton(onPressed:() async {
+                      var username=_usernamecontroller.text;
+                      var password=_passwordcontroller.text;
+                      print("login proceed u:($username) p:($password)");
 
-                  try {
-                      await _plataProvider.get();
+                      Authorization.username=username;
+                      Authorization.password=password;
+                      
+                      try {
+                          var data=await _korisniciProvider.get(filter: {
+                        'KorisnickoIme':username,
+                        }
+                      );
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                // builder: (context) => HomePage(naziv: username,),
+                                builder: (context) => KorisnikDetailsScreen(korisnik: data.result.first,),
+
+                              ),
+                            );
+                        } on Exception catch (e) {
+                          showDialog(context: context, builder: (BuildContext context) => 
+                          AlertDialog(
+                            title: const Text("Error"),
+                            content: Text(e.toString()),
+                            actions: [
+                              TextButton(onPressed: ()=>{
+                                Navigator.pop(context),
+                                _usernamecontroller.text="",
+                                _passwordcontroller.text=""
+                              }, child: const Text("OK"))
+                            ],
+                          ));
+                        }
+                    }, child: const Text("Login")),
+                    const SizedBox(height: 20,),
+
+                    ElevatedButton(onPressed: (){
+                      
                         Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => const PlataListScreen(),
-                          ),
-                        );
-                    } on Exception catch (e) {
-                      showDialog(context: context, builder: (BuildContext context) => 
-                      AlertDialog(
-                        title: const Text("Error"),
-                        content: Text(e.toString()),
-                        actions: [
-                          TextButton(onPressed: ()=>{
-                            Navigator.pop(context),
-                            _usernamecontroller.text="",
-                            _passwordcontroller.text=""
-                          }, child: const Text("OK"))
-                        ],
-                      ));
-                    }
-                }, child: const Text("Login")),
+                        MaterialPageRoute(
+                  
+                        builder: (context) => ContactPage(),
+                            ),
+                            );
+                          }, 
+                    child: const Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                     ),
+                    ),
 
-              ]),
+                    // const SizedBox(height: 20,),
+                    // ElevatedButton(onPressed: (){
+                    //   makePayment();
+                    // }, 
+                    // child: const Text('Go to Payment'),
+                    // )
+
+
+                  ]),
+                ),
+              ),
             ),
-          ),
-        ),
-    );
+        );
   }
+  
+// Future<Korisnik?> authenticateUser(String username, String password) async {
+//   final url = Uri.parse('https://localhost:7181/Korisnik');
+
+//   final response = await http.post(
+//     url,
+//     headers: {'Content-Type': 'application/json'},
+//     body: jsonEncode({'username': username, 'password': password}),
+//   );
+
+//   if (response.statusCode == 200) {
+//     final jsonResponse = jsonDecode(response.body);
+//     return Korisnik.fromJson(jsonResponse);
+//   } else {
+//     return null;
+//   }
+// }
 }
 
+
+
+// Login endpoint
+// Future<String> login(String username, String password) async {
+//   // Perform authentication and validate credentials
+//   bool isValidCredentials = await authenticate(username, password);
+  
+//   if (isValidCredentials) {
+//     // Generate a JWT token
+//     final token = generateJwtToken(username);
+//     return token;
+//   } else {
+//     throw Exception('Invalid credentials');
+//   }
+// }
+
+// // Generate JWT token
+// String generateJwtToken(String username) {
+//   // Generate token with desired payload (e.g., username and expiration time)
+//   final payload = {
+//     'username': username,
+//     'exp': DateTime.now().add(Duration(hours: 1)).millisecondsSinceEpoch ~/ 1000,
+//   };
+
+//   // Sign the token using a secret key
+//   final token = JwtDecoder.encode(payload, 'your_secret_key');
+//   return token;
+// }
 
 
 
