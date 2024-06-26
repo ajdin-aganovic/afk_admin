d62430c5d692ab66af2037e2dda20b55bc0cd017
Ajdin Aganović <89980063+ajdin-aganovic@users.noreply.github.com>
2024-06-26 12:09:02 +0200

    dorada oko reset passworda

--- lib/screens/reset_password_screen.dart
+++ lib/screens/reset_password_screen.dart
@@ -18,6 +18,8 @@ class ContactPage extends StatefulWidget {
   final messageController = TextEditingController();
   final usernameController = TextEditingController();
 
+  final ScrollController _horizontal = ScrollController(), _vertical=ScrollController();
+
   Future sendEmail()async{
     final url=Uri.parse("https://api.emailjs.com/api/v1.0/email/send");
     const serviceId="service_smd9uuu";
@@ -44,7 +46,8 @@ class ContactPage extends StatefulWidget {
 class _ContactPageState extends State<ContactPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return 
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
 
@@ -54,72 +57,116 @@ class _ContactPageState extends State<ContactPage> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Form(
-            child: Column(children: [
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  // icon: const Icon(Icons.account_circle),
-                  labelText: 'Name',
-                  hintText: 'Name'),
-              ),
-              const SizedBox(height: 25,),
-              TextFormField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                  // icon: const Icon(Icons.account_circle),
-                  labelText: 'Username',
-                  hintText: 'Username'),
-              ),
-              const SizedBox(height: 25,),
-              TextFormField(
-                controller: messageController,
-                decoration: const InputDecoration(
-                  // icon: const Icon(Icons.account_circle),
-                  labelText: 'Message',
-                  hintText: 'Message'),
-              ),
-              const SizedBox(height: 25,),
-              TextFormField(
-                controller: subjectController,
-                decoration: const InputDecoration(
-                  // icon: const Icon(Icons.account_circle),
-                  labelText: 'Subject',
-                  hintText: 'Subject'),
-              ),
-              const SizedBox(height: 25,),
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  // icon: const Icon(Icons.account_circle),
-                  labelText: 'Email',
-                  hintText: 'Email'),
-              ),
-              const SizedBox(height: 25,),
-              ElevatedButton(onPressed: (){
-                sendEmail();
-                Navigator.pop(context);
-              }, child: const Text("Pošalji", 
-              style: TextStyle(fontSize: 20),)),
-              const SizedBox(height: 25,),
-              ElevatedButton(onPressed: (){
-                Navigator.of(context).pushAndRemoveUntil<void>
-                (
-                
-                MaterialPageRoute<void>
-                (builder: (BuildContext context) => 
-                LoginPage()),
-                ModalRoute.withName('/Korisnik'),
-                  // MaterialPageRoute(
-                  //   builder: (context) => LoginPage(),
-                  // ),
-                );
-              }, child: const Text("Nazad na login", 
-              style: TextStyle(fontSize: 20),))
-            ]),
-          ),
-        ),
+            child: Center(
+              child: SizedBox(
+                      height: 500,
+                      width: 800,
+              // child: Scrollbar(
+              //   controller: _vertical,
+              //   thumbVisibility: true,
+              //   trackVisibility: true,
+              //   child: Scrollbar(
+              //     controller: _horizontal,
+              //   thumbVisibility: true,
+              //   trackVisibility: true,
+              //   notificationPredicate: (notif)=> notif.depth==1,
+              //   child: SingleChildScrollView(
+              //     controller: _vertical,
+              //     scrollDirection: Axis.vertical,
+              //     child: SingleChildScrollView(
+              //       controller: _horizontal,
+              //       scrollDirection: Axis.horizontal,
+                        child: Form(
+              child: Column(children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      // icon: const Icon(Icons.account_circle),
+                      labelText: 'Name',
+                      hintText: 'Name'),
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: TextFormField(
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                      // icon: const Icon(Icons.account_circle),
+                      labelText: 'Username',
+                      hintText: 'Username'),
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: TextFormField(
+                    controller: messageController,
+                    decoration: const InputDecoration(
+                      // icon: const Icon(Icons.account_circle),
+                      labelText: 'Message',
+                      hintText: 'Message'),
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: TextFormField(
+                    controller: subjectController,
+                    decoration: const InputDecoration(
+                      // icon: const Icon(Icons.account_circle),
+                      labelText: 'Subject',
+                      hintText: 'Subject'),
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      // icon: const Icon(Icons.account_circle),
+                      labelText: 'Email',
+                      hintText: 'Email'),
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: ElevatedButton(onPressed: (){
+                    sendEmail();
+                    nameController.text="";
+                    subjectController.text="";
+                    emailController.text="";
+                    messageController.text="";
+                    usernameController.text="";
+                    Navigator.pop(context);
+                  }, child: const Text("Pošalji", 
+                  style: TextStyle(fontSize: 20),)),
+                ),
+                const SizedBox(height: 25,),
+                Expanded(
+                  child: ElevatedButton(onPressed: (){
+                    Navigator.of(context).pushAndRemoveUntil<void>
+                    (
+                    
+                    MaterialPageRoute<void>
+                    (builder: (BuildContext context) => 
+                    LoginPage()),
+                    ModalRoute.withName('/Korisnik'),
+                      // MaterialPageRoute(
+                      //   builder: (context) => LoginPage(),
+                      // ),
+                    );
+                  }, child: const Text("Nazad na login", 
+                  style: TextStyle(fontSize: 20),)),
+                )
+              ]),
+                        ),
+                      ),
+            ),
       ),
+    ),
+      //   ),
+      //     ),
+      // ),
+      // ),
     );
 
   }
